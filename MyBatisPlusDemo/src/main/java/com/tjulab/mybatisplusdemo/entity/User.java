e9d866291e16b91601cad301101fe52e7583f06b
ShiYang <Shishishi996@163.com>
2022-06-05 15:29:52 +0800

    主键生成策略介绍

--- MyBatisPlusDemo/src/main/java/com/tjulab/mybatisplusdemo/entity/User.java
+++ MyBatisPlusDemo/src/main/java/com/tjulab/mybatisplusdemo/entity/User.java
@@ -1,9 +1,12 @@
 package com.tjulab.mybatisplusdemo.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class User {
+    @TableId(type = IdType.ID_WORKER)
     private Long id;
     private String name;
     private Integer age;
